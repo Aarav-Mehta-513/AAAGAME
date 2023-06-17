4de35d314e14b7a322677c1c388e0cca10814b95
aarush1023 <86903630+FabricMcGaming@users.noreply.github.com>
2023-06-17 15:59:43 -0700

    added tileset

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -2,7 +2,19 @@ import de.gurkenlabs.litiengine.Game;
 
 public class Program {
     public static void main(String[] args) {
+        // meta info
+        Game.info().setName("Super cool game");
+        Game.info().setSubTitle("Placeholder");
+        Game.info().setVersion("v0.0.1");
+        Game.info().setWebsite("https://github.com/Aarav-Mehta-513/AAAGAME");
+        Game.info().setDescription("A super cool side scroll shooter(placeholder)");
+
+        // init
         Game.init(args);
+
+        //scales rendering by 4x
+        Game.graphics().setBaseRenderScale(4f);
+
         Game.start();
     }
 }
